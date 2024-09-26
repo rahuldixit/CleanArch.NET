50b73b72f12ada806699dcd6f68e3cb341ae45f5
Rahul Dixit <rahul.dixit.zero@gmail.com>
2024-09-27 09:16:20 +1000

    mediator

--- CleanArch.NET/CleanArch.Application/ViewModels/CourseViewModel.cs
+++ CleanArch.NET/CleanArch.Application/ViewModels/CourseViewModel.cs
@@ -9,6 +9,10 @@ namespace CleanArch.Application.ViewModels
 {
     public class CourseViewModel
     {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+
         public IEnumerable<Course>? Courses { get; set; }
     }
 }
--- CleanArch.NET/CleanArch.MVC/Program.cs
+++ CleanArch.NET/CleanArch.MVC/Program.cs
@@ -1,9 +1,14 @@
 using CleanArch.Application.Interfaces;
 using CleanArch.Application.Services;
+using CleanArch.Domain.CommandHandlers;
+using CleanArch.Domain.Commands;
+using CleanArch.Domain.Core.Bus;
 using CleanArch.Domain.Interfaces;
+using CleanArch.Infra.Bus;
 using CleanArch.Infra.Data.Context;
 using CleanArch.Infra.Data.Repository;
 using CleanArch.MVC.Data;
+using MediatR;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.EntityFrameworkCore;
@@ -23,8 +28,16 @@ builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.Requ
     .AddEntityFrameworkStores<ApplicationDbContext>();    
 builder.Services.AddRazorPages();
 
+
+builder.Services.AddScoped<IMediatorHandler, InMemoryBus>();
+builder.Services.AddScoped<IRequestHandler<CreateCourseCommand, bool>, CourseCommandHandler>();
+
+
 builder.Services.AddScoped<ICourseService, CourseService>();
 builder.Services.AddScoped<ICourseRepository, CourseRepository>();
+
+builder.Services.AddScoped<UniversityDBContext>();
+
 builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
 
 var app = builder.Build();
